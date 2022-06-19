dbaccd47fbc0239a43705fb4af11084fd2fe6d4b
Osny Neto <osnynt@gmail.com>
2022-06-18 23:05:23 -0300

    first commit

---
+++ Introducao_ReactJS/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Inovation One</p>
+      <p>Bem vindo Osny</p>
+      <div>
+        <div>
+          <div>
+            <p>Osny</p>
+            <h3>Machado</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
